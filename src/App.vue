8795c246e03d67c6a5b01170c9ba3b737be0f211
oleksiishaposhnikov <oshap1044@gmail.com>
2022-06-19 10:29:05 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ARow :gutter="{ sm: 8 }">
+    <ACol :span="12">
+      test
+    </ACol>
+    <ACol :span="12">
+      test 2
+    </ACol>
+  </ARow>
+</template>
